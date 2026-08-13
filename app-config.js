1e5fdce43e190ec66da45b98c563144bd6ad617c
pablojasa <166968356+pablojasa@users.noreply.github.com>
2026-08-13 12:14:31 -0300

    Add files via upload

--- app-config.js
+++ app-config.js
@@ -1,3 +1,3 @@
 // ITB 2.0
-window.ITB_API_URL = "https://script.google.com/macros/s/AKfycbx5rjJuerIcltRdcU934ncFm7JNSpWylluCWjka3rtIIuSBV-rdNZRNYVrfVFWq8v4/exec";
+window.ITB_API_URL = "https://script.google.com/macros/s/AKfycbzvyQK2JJeF0qN9Din8q9CDTeuPWS8Mx2_JdHvOvF5rVp175F40DPsEBTl3pmEDyuax/exec";
 window.ITB_API_TIMEOUT_MS = 12000;
