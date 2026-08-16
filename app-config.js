ea076b761daf30523bfdada26ec838d0c40a9b89
pablojasa <166968356+pablojasa@users.noreply.github.com>
2026-08-16 12:20:19 -0300

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,3 +1,3 @@
 // ITB 2.0
-window.ITB_API_URL = "https://script.google.com/macros/s/AKfycbzvyQK2JJeF0qN9Din8q9CDTeuPWS8Mx2_JdHvOvF5rVp175F40DPsEBTl3pmEDyuax/exec";
+window.ITB_API_URL = "https://script.google.com/macros/s/AKfycbxMj3T9zCu7R3URRkwode-Xbm7SSuuc79C760Fm1WQRiKYCsVu3bRo5Y1xPzQVhGnsVpw/exec";
 window.ITB_API_TIMEOUT_MS = 12000;
