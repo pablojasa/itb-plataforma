a2d719bbb7386ca06529e82fb51639fd7d96d6ef
pablojasa <166968356+pablojasa@users.noreply.github.com>
2026-08-18 14:18:38 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='itb-pwa-v0340';
+const CACHE_NAME='itb-pwa-v0.35.0';
 const SHELL=['./','./index.html','./manifest.webmanifest','./assets/ted-app-icon-180-v0281.png','./assets/ted-app-icon-192-v0281.png','./assets/ted-app-icon-512-v0281.png'];
 
 self.addEventListener('install',event=>{
